b2d9a565cb63e9208e5f7238a82f011b49d1ae0f
Nathaniel Rubin <narubin@umich.edu>
2014-02-22 17:24:25 -0500

    Sexy focus for rows

--- ourScript.js
+++ ourScript.js
@@ -1,3 +1,5 @@
+var first_click = false;
+
 $(document).ready(function() {
   function loadTransportData() {
     console.log(3);
@@ -6,7 +8,29 @@ $(document).ready(function() {
   $(".outer_row").click( function() {
     if ($(this).hasClass("focus"))
       return;
-    $("div.outer_row.focus").css("background-color", "").removeClass("focus");
+    /*$("div.outer_row.focus").css("background-color", "").animate({"padding-left":"-=40px"})
+    $("div.outer_row.focus").fadeTo("fast", 1).removeClass("focus");
+
     $(this).addClass("focus");
+    $(this).animate({"padding-left":"+=40px"});
+    $("div.outer_row").not(".focus").fadeTo("fast", .3);*/
+
+    $(this).addClass("dummy");
+    $(".outer_row").each(function() {
+      if (first_click && $(this).hasClass("focus")) {
+        $(this).css("background-color", "")
+        $(this).animate({"padding-left":"-=40px", "opacity":".5"})
+        $(this).removeClass("focus");
+      }
+      else if (!first_click && !$(this).hasClass("dummy")) {
+        $(this).animate({"opacity":".5"})
+      }
+    });
+
+    first_click = true;
+    $(this).removeClass("dummy");
+    $(this).addClass("focus");
+    $(this).animate({"padding-left":"+=40px", "opacity":"1"});
+    //$(this).fadeTo("fast", 1)
   });
 });
